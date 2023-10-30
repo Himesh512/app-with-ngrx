b4240fa90c6559c02d483f9430260162f6f3b879
HimsM5 <himesh.aadeshara@marutitech.com>
2023-10-31 00:14:37 +0530

    date picker changes

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,10 +3,26 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatIconModule } from "@angular/material/icon";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from "@angular/material/core";
 
 @NgModule({
-    imports:[ MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule ],
-    exports:[ MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule ]
+    imports:[ 
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        MatDatepickerModule,
+        MatNativeDateModule
+    ],
+    exports:[ 
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        MatDatepickerModule,
+        MatNativeDateModule 
+    ]
 })
 
 export class MaterialModule {
