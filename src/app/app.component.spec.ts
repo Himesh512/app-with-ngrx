a733254af6c1ae3f04752607013b828a757ed382
HimsM5 <himesh.aadeshara@marutitech.com>
2023-11-01 00:50:54 +0530

    app deployment to github page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'full-app-with-ngrx'`, () => {
+  it(`should have as title 'app-with-ngrx'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('full-app-with-ngrx');
+    expect(app.title).toEqual('app-with-ngrx');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('full-app-with-ngrx app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('app-with-ngrx app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'full-app-with-ngrx';
+  title = 'app-with-ngrx';
 }
